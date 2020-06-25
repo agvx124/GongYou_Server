2ac946df1a7cfcae1fe8a3c508a0649c050481bc
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2020-06-25 11:41:20 +0900

    [ADD] GetFiles

--- src/api/v1/upload/index.js
+++ src/api/v1/upload/index.js
@@ -4,5 +4,6 @@ const fileCtrl = require('./upload.ctrl');
 const fileRouter = new Router();
 
 fileRouter.post('/urlUpload', fileCtrl.urlUpload);
+fileRouter.get('/getFiles/:fileEigenValue', fileCtrl.getFiles);
 
 module.exports = fileRouter;
--- src/api/v1/upload/upload.ctrl.js
+++ src/api/v1/upload/upload.ctrl.js
@@ -45,3 +45,54 @@ exports.urlUpload = async (ctx) => {
         console.log(ex);
     }
 }
+
+exports.getFiles = async (ctx) => {
+    try {
+        const { fileEigenValue } = ctx.params;
+
+        if (!fileEigenValue) {
+            ctx.status = 400;
+            ctx.body = {
+                status: 400,
+                message: '파일 고유 번호가 없습니다.'
+            }
+            return;
+        }
+
+        let files = await model.fileInfo.findAll({
+            where : {
+                fileEigenValue : fileEigenValue,
+            }
+        });
+
+        if (!files) {
+            ctx.status = 404;
+            ctx.body = {
+                status: 404,
+                message: '고유 번호와 매칭되는 파일이 없습니다.'
+            }
+            return;
+        }
+
+        let fileUrlList = new Array;
+        for (let i = 0; i < files.length; i ++) {
+            fileUrlList.push(files[i].fileExt + "/" + files[i].filesUrl);
+        }
+
+        ctx.status = 200;
+        ctx.body = {
+            status: 200,
+            message: '파일 다운 URL 조회 성공',
+            data: fileUrlList
+        }
+
+
+    } catch (ex) {
+        ctx.status = 500;
+        ctx.body = {
+            status: 500,
+            message: '서버 오류'
+        };
+        console.log(ex);
+    }
+}
